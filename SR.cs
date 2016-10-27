733dc4585f3c688aebe3c68e52b535cb1d06233a
Dorian Stoll <dorian@tmsp.ga>
2016-10-27 12:51:15 +0200

    Added error messages

---
+++ SR.cs
@@ -0,0 +1,10 @@
+﻿namespace System.Regex
+{
+    internal class SR : SR_Internal
+    {
+        internal static string GetString(string s, params object[] args)
+        {
+            return String.Format(s, args);
+        }
+    }
+}
