acbf1d3b5d52005cc9ec20ac605d47660c22d319
Dorian Stoll <dorian@tmsp.ga>
2016-10-27 12:28:46 +0200

    Code dump from referencesource

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("System.Regex")]
+[assembly: AssemblyDescription("Implementation of System.Text.RegularExpressions for Unity3d that supports compiled Regexes")]
+[assembly: AssemblyProduct("System.Regex")]
+[assembly: AssemblyCopyright("Copyright © Microsoft Coorperation 2016")]
+[assembly: ComVisible(false)]
+[assembly: Guid("3992bd2c-c1ca-438d-a8bf-d58210a4d0aa")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# System.Regex
-System.Text.RegularExpressions implementation for Unity3d with support for compiled regex
+### System.Regex
+This is a backport of Microsofts System.Text.RegularExpressions implementation to .NET 3.5, found here: https://github.com/Microsoft/referencesource/tree/master/System/regex/system/text/regularexpressions/
+All credit goes to Microsoft
+
